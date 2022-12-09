00e2e19bdd02831281f85f7df75c6e2fc586e76f
LimGwangSu <didth0812@gmail.com>
2022-12-09 23:37:50 +0900

    docker-compose 생성 -> postgreSQL

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { setupSwagger } from './utils/setupSwagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   setupSwagger(app);
 
   await app.listen(3000);
